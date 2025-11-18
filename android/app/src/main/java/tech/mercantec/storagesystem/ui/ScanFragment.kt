5fec18ab4df3be79ab497d06067eccdd8ab91bea
Reimar <mail@reim.ar>
2025-11-18 10:34:03 +0100

    Show camera

--- android/app/src/main/java/tech/mercantec/storagesystem/ui/ScanFragment.kt
+++ android/app/src/main/java/tech/mercantec/storagesystem/ui/ScanFragment.kt
@@ -1,21 +1,76 @@
 package tech.mercantec.storagesystem.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
 import tech.mercantec.storagesystem.R
 
 class ScanFragment : Fragment() {
+    private val CAMERA_PERMISSION_REQUEST_CODE = 200
+    private lateinit var previewView: PreviewView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val cameraPermission = checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+
+        if (cameraPermission == PERMISSION_GRANTED) {
+            showCamera()
+        } else {
+            requireActivity().requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String?>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults.contains(PERMISSION_GRANTED)) {
+                showCamera()
+            } else {
+                // TODO handle rejection
+            }
+
+            return
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun showCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().apply { surfaceProvider = previewView.surfaceProvider }
+
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, preview)
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_scan, container, false)
+        val view = inflater.inflate(R.layout.fragment_scan, container, false)
+
+        previewView = view.findViewById(R.id.preview)
+
+        return view
     }
 }
